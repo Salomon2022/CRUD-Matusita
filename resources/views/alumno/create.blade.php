541e933210467ae756134aa10e51232fa5826e69
Salomon2022 <1386838@senati.pe>
2023-01-25 13:14:39 -0500

    creamos formulario de registro de alumnos Matusita

--- resources/views/alumno/create.blade.php
+++ resources/views/alumno/create.blade.php
@@ -1,3 +1,32 @@
 Formulario creacion de alumnos
-/*envío de archivos*/
-<form action="" method="post"enctype="multipart/form-data">
+MATUSITA 
+<form action="{{ url('/alumno')}}" method="post"enctype="multipart/form-data">
+
+<label for="Nombre"> Nombre </label>
+<input type="text" name="Nombre" id="Nombre">
+<br>
+
+<label for="ApellidoPaterno">Apellido Paterno</label>
+<input type="text" name="ApellidoPaterno" id="ApellidoPaterno">
+<br>
+
+<label for="ApellidoMaterno">Apellido Materno</label>
+<input type="text" name="ApellidoMaterno" id="ApellidoMaterno">
+<br>
+
+<label for="Grado_Secccion">Grado_Secccion</label>
+<input type="text" name="Grado_Seccion" id="Grado_Seccion">
+<br>
+
+<label for="Correo">Correo</label>
+<input type="text" name="Correo" id="Correo">
+<br>
+
+<label for="Foto">Foto</label>
+<input type="file" name="Foto" id="Foto">
+<br>
+
+<input type="submit" name="Enviar" id="Enviar">
+<br>
+
+</form>
